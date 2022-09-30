ee0ae0c090bd82fa91913a1440ad90ac6c21f7df
vmendesd <114742166+vmendesd@users.noreply.github.com>
2022-09-30 08:30:37 -0300

    Add files via upload

---
+++ questao01.py
@@ -0,0 +1,7 @@
+lado = int(input('Qual o valor de um dos lados do retângulo?'))
+ladu = int(input('Qual o valor do outro lado do retângulo?'))
+perimetro = lado + lado + ladu + ladu
+area = lado * ladu
+print(f'O valor do perímetro é {perimetro}')
+print(f'A área é {area}')
+
